1d052ab579b42286a8e5d0be28d78b6b508e410a
Katerina <katerinanikou@gmail.com>
2017-12-22 16:55:52 +0100

    a few changes
    skapat homepage_controller
    lagt in lite kod och testat

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-index.php
+<?php
+// create session_start();
+session_start();
+//session_unset();
+//session_destroy();
+
+
+if (!isset($_SESSION['cart']))
+  $_SESSION['cart'] = array();
+
+
+$routes = array(
+'default' => 'homepage_controller.php',
+);
+
+if(isset($_GET['action'])){
+    if (is_file('controllers/'.$routes[$_GET['action']])){
+        require('controllers/'.$routes[$_GET['action']]);
+    } else {
+    	
+        require('templates/404.php');
+    }
+    
+} else {
+    require('controllers/'.$routes['default']);
+}
---
+++ templates/default.php
@@ -0,0 +1,12 @@
+<?php
+require('templates/header.php');
+?>
+
+<h1><?php echo $pagecontent->title; ?></h1>
+
+<?php
+require('templates/footer.php');
+?>
+
+<?php require_once('templates/footer.php');
+?>
