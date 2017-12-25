a93c0407cc6da05b8bc60ed1f3e5f1a4d878fe89
Katerina <katerinanikou@gmail.com>
2017-12-25 22:52:33 +0100

    404 error page template
    Start of making 404 error page

--- templates/404.php
+++ templates/404.php
@@ -1,1 +1,9 @@
-404.php
+<?php
+require('templates/header.php');
+	
+?>
+<!--<link type="text/css" rel="stylesheet" href="css/404.css">-->
+<h1>Ciao! Sidan du försöker hitta finns inte. </h1>
+
+<?php 
+require('templates/footer.php');
