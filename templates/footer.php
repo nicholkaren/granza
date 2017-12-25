6ac865b1eb1a566968ceb228b1403ebb5171cf77
Katerina <katerinanikou@gmail.com>
2017-12-25 21:47:50 +0100

    footer template and css
    footer template and css

--- templates/footer.php
+++ templates/footer.php
@@ -1,1 +1,41 @@
-footer.php
+<!--FOOTER-->
+</body>
+
+<footer>
+
+    <!-- De 3 blocken med innehåll -->
+    <div class="footerwrap wrap">
+
+    <!-- 1:a blocket med beskrivande text -->
+    <div class="square eq">
+        <p class="granza-foooter-text">
+        Granza Perfume is a Italian luxury perfume company, 
+        with craftsmanship at its heart. With a passion for 
+        transforming the expected into the extraordinary, 
+        Granza’s perfumes are uniquely complex and expertly 
+        crafted with the rarest, most precious ingredients.
+    </p>
+    </div>
+    
+    <!--2:a blocket med menylänkar -->
+    <div class="square eq">
+        <p class="granza-foooter-links">
+            <a href="?action=category&id=1">Damdofter</a><br>
+            <a href="?action=category&id=2">Herrdofter</a><br>
+            <a href="?action=about-us">Om oss</a><br>
+            <a href="?action=about-us">FAQ</a><br>
+        </p>
+    </div>
+    
+    <!--3:e blocket med kommande bildkollage -->
+    <div class="square eq">
+        <p class="granza-foooter-text">
+        Det lilla fotokollaget placeras här, om vi vill ha det.
+        </p>
+        <p id="copyright">© Granza 2018 </p>
+        <!-- <img src="img/products/doftnoter/liljekonvalj.png" height="500" width="400"></a>-->
+    </div>
+  
+    </div>
+</footer>
+</html>
