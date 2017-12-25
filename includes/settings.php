05edbb697933d304c562f7b76f975a0e4e30e177
Katerina <katerinanikou@gmail.com>
2017-12-25 22:48:32 +0100

    settings for database
    database notes

---
+++ includes/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+// Inloggning till databasen
+$settings['db']['user'] = 'root';
+$settings['db']['password'] = 'root';
+$settings['db']['host'] = 'localhost';
+$settings['db']['database'] = 'granza';
