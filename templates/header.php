98eeb26a8d40f3e8e3c5d63a03f12c8422804938
Katerina <katerinanikou@gmail.com>
2017-12-25 22:42:03 +0100

    header css and template
    The start of making the header

---
+++ templates/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Granza perfumes</title>
+<link rel="shortcut icon" href="img/logo/granza.png"/>
+<link type="text/css" rel="stylesheet" href="css/header.css">
+<link type="text/css" rel="stylesheet" href="css/footer.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800|Work+Sans:300,400" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">  
+
+</head>
+<body>
+<!--HEADER-->
+<header>
+
+    <!--MENY SOM FÄLLS UT-->
+      <input type="checkbox" id="nav-trigger" class="nav-trigger">
+        <label for="nav-trigger" class="toggle">☰</label>
+        <nav class="menu">
+            <h1 id="menu-button">Meny</h1>
+            
+            <ul class="ulmenu">    
+                <li><a href="?action=category&id=1">Damdofter</a></li>
+                <li><a href="?action=category&id=2">Herrdofter</a></li>
+                <li><a href="?action=about-us">Om oss</a></li>
+                <li><a href="?action=faq">FAQ</a></li>
+            </ul>
+        </nav>
+    
+    <!--LOGOTYP-->
+    <div class="div_logo">
+        <a href="?action=default">
+        <img src="img/logo/granza.png" alt="logotype granza" id="id_logo"></a>
+    </div>
+    
+     
+</header>
