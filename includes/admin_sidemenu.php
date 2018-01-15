1dc0f65ae47ddee1e75038063307337076ecab92
Nicholbot <nichololguin89@gmail.com>
2018-01-15 13:41:47 +0100

    Påbörjar form på skapa product

--- includes/admin_sidemenu.php
+++ includes/admin_sidemenu.php
@@ -2,17 +2,29 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<!--SIDO MENY I ADMIN-->
+<!--SIDO MENY I ADMIN FÖR PRODUKTER GRÖN-->
 <nav class="side_menu" tabindex="0">
-	<div class="smartphone-menu-trigger"></div>
   <header class="big_icon">
 		<img class="side_menu_icon" src="img/icons/perfume_color.png" />
     <h2 class="side_title">Produkter</h2>
   </header>
-	<ul class="second_menu">
-    <li class="side_menu"><span>Skapa ny produkt</span><i class="fa fa-angle-right"></i></li>
-    <li  class="side_menu"><span>Redigera produkt</span><i class="fa fa-angle-right"></i></li>
-    <li  class="side_menu"><span>Radera produkt</span><i class="fa fa-angle-right"></i></li>
-    <li  class="side_menu_back"><span><i class="fa fa-angle-left"></i>Tillbaka</span></li>
-  </ul>
+        <ul class="second_menu">
+            <li class="side_menu">
+                    <a href="?action=create-product" class="txt">Skapa ny produkt</a>
+                        <i class="fa fa-angle-right"></i>
+            </li>
+
+            <li class="side_menu">
+                    <a href="?action=edit-product"class="txt">Redigera produkt</a>
+                        <i class="fa fa-angle-right"></i>
+            </li>
+
+            <li class="side_menu">
+                    <a href="?action=all-products" class="txt">Visa produkter</a>
+                        <i class="fa fa-angle-right"></i>
+            </li>
+            <li  class="side_menu">
+                <i class="fa fa-angle-left"></i><a href="?action=login" class="txt1">Tillbaka</a>
+            </li>
+      </ul>
 </nav>
--- templates/add_product_tpl.php
+++ templates/add_product_tpl.php
@@ -1,39 +1,49 @@
+<!--ADMIN SKAPA PRODUKT SIDA-->
 <?php require_once('templates/admin_header.php');?>
 <?php require_once('includes/admin_sidemenu.php');?>
 
 <link type="text/css" href="css/add_product.css" rel="stylesheet"> 
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 
-<h1 id="add-product-h1">SKAPA NY PRODUKT</h1>
-<hr>
-
-<?php if ($pagecontent->error !== ""){
-    echo '<p id="p-error">'.$pagecontent->error.'</p>';
-} else if (!empty($last_id)) {
-    echo '<p id="p-success">Produkt tillagd!</p>';
-    echo '<h2>Produktens id är: '.$last_id.'</h2>';
-}?>
-
-<form method="post" action="?action=create-product" enctype="multipart/form-data" id="create-product" name="add-product">
-    <input type="file" name="file1" id="file-upload">
-    <input type="text" name="product[title]" id="prod-title" placeholder="Produkttitel">
-    <input type="text" name="product[price]" id="prod-price" placeholder="Pris">
-    <textarea form="create-product" rows="10" cols="50" name="product[desc]" id="prod-desc" placeholder="Produktbeskrivning"></textarea>
-    <p>Ange kategori:</p>
-    <select name="product[cat_id]">
-        <?php foreach ($pagecontent->category as $currcat) {
-            echo '<option value="'.$currcat["cat_id"].'">'.$currcat['title'].'</option>';
-        }?>
-    </select>
-    <p>Ange status:</p>
-    <select name="product[status]">
-        <option value="active" name="product[active]">Aktiv</option>
-        <option value="inactive" name="product[inactive]">Inaktiv</option>
-    </select>
-    <!-- FORM TOKEN-->
-    <?php echo getTokenField(); ?>
+<!--Här börjar hela innehållet-->
+<main class="create_product">
     
-    <button type="submit" id="save-product-button" name ="save-product">Spara produkt</button>
+    <!--Titel-->
+    <h1 id="add-product-h1">Skapa ny produkt</h1>
     
-</form>
+    <!-- Här matas det sparade PID för produkten eller ERROR om det ej matas in i DB -->
+    <?php if ($pagecontent->error !== ""){
+        echo '<p id="p-error">'.$pagecontent->error.'</p>';
+    } else if (!empty($last_id)) {
+        echo '<p id="p-success">Produkt tillagd!</p>';
+        echo '<h2>Produktens id är: '.$last_id.'</h2>';
+    }?>
+<!--Form för att mata in nya produkter i DB-->
+    <form method="post" action="?action=create-product" enctype="multipart/form-data" id="create-product" name="add-product">
+        <input type="file" name="file1" id="file-upload">
+        <input type="text" name="product[title]" id="prod-title" placeholder="Produkttitel">
+        <input type="text" name="product[price]" id="prod-price" placeholder="Pris">
+        <textarea form="create-product" rows="10" cols="50" name="product[desc]" id="prod-desc" placeholder="Produktbeskrivning"></textarea>
 
+<!--Här anger admin kategori för PID-->        
+        <p>Ange kategori:</p>
+        <select name="product[cat_id]">
+            <?php foreach ($pagecontent->category as $currcat) {
+                echo '<option value="'.$currcat["cat_id"].'">'.$currcat['title'].'</option>';
+            }?>
+        </select>
+    
+<!-- Väljer status för PID i DB-->
+        <p>Ange status:</p>
+        <select name="product[status]">
+            <option value="active" name="product[active]">Aktiv</option>
+            <option value="inactive" name="product[inactive]">Inaktiv</option>
+        </select>
+        
+        <!-- FORM TOKEN-->
+        <?php echo getTokenField(); ?>
+
+        <button type="submit" id="save-product-button" name ="save-product">Spara produkt</button>
+
+    </form>
+</main>
