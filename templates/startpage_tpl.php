069f5dac947b07dfe73a1bd86b22d3c681fce51c
katerinanikou <katerinanikou1@gmail.com>
2018-01-15 22:32:44 +0100

    Startsida 1.0 klar
    Har gjort en ny hover på produktbilder
    Ändrat upplägget

--- templates/startpage_tpl.php
+++ templates/startpage_tpl.php
@@ -14,20 +14,18 @@
         <i class="fa fa-bolt" aria-hidden="true"></i> Snabb leverans! | 
         <i class="fa fa-smile-o" aria-hidden="true"></i> Över 20 000 nöjda kunder | 
         <i class="fa fa-gift" aria-hidden="true"></i> Parfymprov vid varje beställning </div> 
-    
-    <div class="startpage">
-        <!-- TOPPBILD -->
+            <!-- TOPPBILD -->
         <div class="startpage_bigimage">
             <img class="topimage" src="img/slider/big_img.jpg">  
-            <a href="#">
-                <button id="create"> Begin fragrance profiling </button>
-            </a>
+                <a href="#">
+                    <button id="create"> Begin fragrance profiling </button>
+                </a>
         </div>
-
+        
+        
+        <div class="startpage">
         <!-- FÖRSTA PRODUKTRADEN MED DAMDOFTER -->
-        <a href="?action=category&id=1">
-            <h2 class="headline_gender"> Damdofter | Profumi donna </h2>
-        </a>
+        <a href="?action=category&id=1"> <h2 class="headline_gender"> Damdofter | Profumi donna </h2></a>
         
         <div class="start_prod">
             <?php foreach ($pagecontent->products as $currprod) {
@@ -35,7 +33,6 @@
                 echo '<a href="?action=product&pid='.$currprod['product_id'].'">';
                 echo '<img src="'.$currprod['img_url'].'" class="prod_img">';
                 echo '<h3 class="perfume_name">'.$currprod['title'].'</h3>';
-                echo '<p class="price">'.$currprod['price'] .' sek'.'</p>';
                 echo '</a>';
                 echo '<form method="post" action="?action=addtocart">';
                 echo '<input type="hidden" value="'.$currprod['price'].'" name="price">';
@@ -47,23 +44,14 @@
             } ?>
         </div>
 
-        <div class="start_prod">
-            <?php foreach ($pagecontent->products as $currprod) {
-                echo '<div class="product_wrapper">';
-                echo '<a href="?action=product&pid='.$currprod['product_id'].'">';
-                echo '<img src="'.$currprod['img_url'].'" class="prod_img">';
-                echo '<h3 class="perfume_name">'.$currprod['title'].'</h3>';
-                echo '<p class="price">'.$currprod['price'] .' sek'.'</p>';
-                echo '</a>';
-                echo '<form method="post" action="?action=addtocart">';
-                echo '<input type="hidden" value="'.$currprod['price'].'" name="price">';
-                echo '<input type="hidden" value="1" id="cart_qty" name="qty">';
-                echo '<input type="hidden" value="'.$currprod['product_id'].'" id="cart_" name="pid">';
-                echo '<button type="submit">Lägg i varukorgen</button>';
-                echo '</form>';
-                echo '</div>';
-            } ?>
-        </div>
+        <!-- UPPTÄCK ALLA DAMDOFTER -->
+        <a href="?action=category&id=2">
+        <div class="discover_women">
+            <img src="img/products/big/005_big.jpg">
+            <p class="all_women"> Discover all perfumes <br>for donna </p>
+            <div class="arrow-right"></div>
+        </div> 
+        </a>
 
         <!--Ansök om medlemsskap-->
         <div id="register_member">
@@ -77,21 +65,10 @@
                     <button id="ansok"> Ansök här! </button>
             </div>
         </div>
-        
-
-        <!-- PRODUKTRAD MED HERRDOFTER -->
-        <a href="?action=category&id=2">
-        <div class="discover_men">
-            <img src="img/products/uomo/redigerad_005.png">
-            <p class="all_men"> Discover all perfumes <br>for oumo </p>
-            <div class="arrow-right"></div>
-
-
-        </div> 
-        </a>
 
         <!-- PRODUKTRAD MED HERRDOFTER -->
         <a href="?action=category&id=2"><h2 class="headline_gender">Herrdofter | Profumi Oumo</h2></a>
+            
             <div class="start_prod">
                 <?php foreach ($pagecontent2->products as $currprod2) {
                     echo '<div class="product_wrapper">';
@@ -108,35 +85,17 @@
                     echo '</form>';
                     echo '</div>';
                 } ?>
-        </div> 
-
-         <!-- PRODUKTRAD MED HERRDOFTER -->
-            <div class="start_prod">
-                <?php foreach ($pagecontent2->products as $currprod2) {
-                    echo '<div class="product_wrapper">';
-                    echo '<a href="?action=product&pid='.$currprod2['product_id_2'].'">';
-                    echo '<img src="'.$currprod2['img_url_2'].'" class="prod_img">';
-                    echo '<h3 class="perfume_name">'.$currprod2['title_2'].'</h3>';
-                    echo '<p class="price">'.$currprod2['price_2'] .' KR'.'</p>';
-                    echo '</a>';
-                    echo '<form method="post" action="?action=addtocart">';
-                    echo '<input type="hidden" value="'.$currprod2['price_2'].'" name="price">';
-                    echo '<input type="hidden" value="1" id="cart_qty" name="qty">';
-                    echo '<input type="hidden" value="'.$currprod2['product_id_2'].'" id="cart_" name="pid">';
-                    echo '<button type="submit">Lägg i varukorgen</button>';
-                    echo '</form>';
-                    echo '</div>';
-                } ?>
-        </div> 
+            </div> 
 
         <!--En kreativ yta -->
         <div id="philosophy">
-                <img id="leafs" src="img/doftnoter/leaf_3.png">
                 <p class="thephilosophy"> "In the victorian times, everyone was well versed in the language of perfume. <br> When you gifted a bouquet or a perfume, you were telling a story, or sending a message"
                 <br> - Fransesco Granza</p>
         </div> 
+
         
     </div> <!-- End of startpage div -->
+</div>
 
     <?php require_once('templates/footer.php');
     ?>
