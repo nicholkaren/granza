b6ddfee8dca6bed762eb4b68321c8275508d98df
katerinanikou <katerinanikou1@gmail.com>
2018-01-18 14:05:03 +0100

    Små justeringar i flera filer
    Nothing important

--- templates/header.php
+++ templates/header.php
@@ -8,7 +8,7 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800|Work+Sans:300,400" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1">  
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 <body>
