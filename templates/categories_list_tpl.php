3b41319fabdf120d79c4cc04f5f07262727b42f3
Nicholbot <nichololguin89@gmail.com>
2018-01-20 16:46:15 +0100

    ändringar i admin visa alla kategorier

--- templates/categories_list_tpl.php
+++ templates/categories_list_tpl.php
@@ -1,40 +1,48 @@
-<?php require_once('templates/header.php');?>
-<?php require_once('includes/admin_menu.php');?>
-<link type="text/css" href="css/admin_list.css" rel="stylesheet"> 
+<!--Admin visa alla kategorier-->
+<?php require_once('templates/admin_header.php');?>
+<?php require_once('includes/admin_sidemenu_cat.php');?>
+<link type="text/css" href="css/admin_list.css" rel="stylesheet">
 
-<h1 id="h1-cat"><?php echo strtoupper($pagecontent->title); ?></h1>
-<hr>
+<!--BAKGRUNDBILD-->
+<img class="background" src="img/products/big/marble-wallpaper-white.jpg">
+<!--VITA DIVEN-->
+<div class="all_product">
+    <!--sid titel-->
+    <h1 id="h1-cat">
+        <?php echo strtoupper($pagecontent->title); ?>
+    </h1>
 
-<div id="cat-list-wrapper">
+    <!-- PRINTA ALLA KATEGORIER SOM FINNS I DATABASEN-->
 
-<!-- PRINTA ALLA KATEGORIER SOM FINNS I DATABASEN-->
-      
-<table class="list-wrapper">
-  <tr>
-    <th>Id</th>
-    <th>Bild</th>
-    <th>Titel</th>
-    <th>Beskrivning</th>
-    <th>Status</th>
-    <th>Redigera</th>
-  </tr>
-    <?php foreach ($pagecontent->categories as $currcat) {
-  echo '<tr>';
-    printf ('<td>%s</td>',$currcat['category_id']);
-    printf ('<td><img src="%s" class="cat-list-img"</td>',$currcat['img_url']);
-    printf ('<td>%s</td>',$currcat['title']);
-    printf ('<td class="cat-desc-list">%s</td>',$currcat['description']);
-    echo '<td>'; 
-    if ($currcat['status'] === '1'){
-        echo 'Inaktiv';
-    } else {
-        echo 'Aktiv';
-    }; 
-    echo '</td>';
-    echo '<td><a href="?action=edit-category&id='.$currcat['category_id'].'">Redigera</a></td>';
-  echo '</tr>';
-}?>
-</table>
-    
+    <table class="list-wrapper">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Bild</th>
+                <th>Titel</th>
+                <th>Beskrivning</th>
+                <th>Status</th>
+                <th>Redigera</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($pagecontent->categories as $currcat) {
+              echo '<tr>';
+                printf ('<td>%s</td>',$currcat['category_id']);
+                printf ('<td><img src="%s" class="cat-list-img"</td>',$currcat['img_url']);
+                printf ('<td>%s</td>',$currcat['title']);
+                printf ('<td class="cat-desc-list">%s</td>',$currcat['description']);
+                echo '<td>'; 
+            if ($currcat['status'] === '1'){
+                    echo 'Inaktiv';
+                } else {
+                    echo 'Aktiv';
+                }; 
+                echo '</td>';
+                echo '<td><a href="?action=edit-category&id='.$currcat['category_id'].'">Redigera</a></td>';
+              echo '</tr>';
+            }?>
+        </tbody>
+    </table>
+
 </div>
-<?php require_once('templates/footer.php');?>
