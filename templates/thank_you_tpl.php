6e4c2f5a8a87c95f475a641fdd71bfc2a69ce242
katerinanikou <katerinanikou1@gmail.com>
2018-01-21 23:45:32 +0100

    thank you template html och css

--- templates/thank_you_tpl.php
+++ templates/thank_you_tpl.php
@@ -3,18 +3,15 @@
 <link type="text/css" href="css/thank_you.css" rel="stylesheet"> 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- TACK FÖR DIN ORDER -->
-<div class="thanks">
-
-
-<h1 id="thanks-h1">Tack för ditt köp!</h1>
-<p id="thanks-p">Vi skickar en orderbekräftelse via e-post inom kort. Har du några frågor hör gärna av dig till oss.</p>
-<div id="thanks-img-div">
-    <img src="img/logo/logo-millhouse-black-bold.png" alt="logo" id="thanks-logo">
- </div>   
-    <button type="button" id="to-store"><a href="?action=default">TILL BUTIKEN</a></button>
-
-
+	<img class="background_flowers" src="img/products/big/thankyou.jpg">  
+		<div class="thank_you_wrapper">
+			<h1 id="thanks-h1"> Grazie! Tack för ditt köp!</h1>
+			<p id="thanks-p"> 
+				Vi skickar en orderbekräftelse till dig via e-post inom kort. <br>
+				Har du några frågor så får du gärna höra av dig till oss. <br><br>
+            	<i class="fa fa-phone" aria-hidden="true"></i> <span id="order"> 020 - 53 53 52 </span></li>
+			</p>
+		</div>
 <?php 
     
     require_once('templates/footer.php');
