6efb65d961ad65f2e4b55f13cd777dc2759a2b20
katerinanikou <katerinanikou1@gmail.com>
2018-01-21 17:42:44 +0100

    Cart html och css
    Färdigt utkast
    Allt fungerar med controller

--- templates/parts/cart_tpl.php
+++ templates/parts/cart_tpl.php
@@ -5,84 +5,88 @@ require('templates/header.php');
 <link type="text/css" href="css/cart.css" rel="stylesheet"> 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
+    <div id="fullwidth">
+        <h1 class="shoppingbag"> Din varukorg ( steg 2 ) </h1>
     <!--Här är varukorgen -->
     <div id="cart-wrapper">
-        <p class="shoppingbag">Varukorg</p>
-
-
-    <!-- Formuläret med varorna -->
-    <form method="post" action="?action=updatecart" id="update-cart-form">
+        
+        <!-- Formuläret med varorna -->
+        <form method="post" action="?action=updatecart" id="update-cart-form">
      
-    <!-- Content -->   
-    <div class="cart-content">
-        <?php if (!array_key_exists('cartItems', $cart)){
-            print '<p id="empty-cart">Din varukorg är tom!</p>';         
-    };?>
-        <?php if (isset($cart['cartItems'])):
-        foreach ($cart['cartItems'] as $cartItemPid => $cartItemData): ?>
-       
+            <!-- Content -->   
+            <div class="cart-content">
+                <?php if (!array_key_exists('cartItems', $cart)){
+                    print '<p id="empty-cart">Din varukorg är tom!</p>';         
+                };?>
+                <?php if (isset($cart['cartItems'])):
+                foreach ($cart['cartItems'] as $cartItemPid => $cartItemData): ?>
+           
+                <!-- Omsluter en produkt -->
+                <div class="item">
 
-
-
-        <div class="item">
-
-        <div class="item-img">
-            <img id="cart_img" src="<?php echo $cartItemData['img_url'];?>">
-        </div>
-                    
-        <div class="item-title">
-            <span class="desc"><?php echo $cartItemData['title'];?></span>
-        </div>
-                    
-        <div class="item-qty">
-            <input type="number" name="cartitems[<?php echo $cartItemPid;?>]" value="<?php echo $cartItemData['qty'];?>">
-        </div>
-                    
-        <div class="item-price">
-            <?php echo $cartItemData['price'].' KR';?>
-        </div>
-                    
-        <div class="cart-sum">
-            <?php echo $cartItemData['sum'].' KR';?>
-
-              <a href="?action=removecartitem&pid=<?php echo $cartItemPid;?>">
-                <i class="fa fa-trash" aria-hidden="true"></i>
-
-                </a>
-        </div>
-
-
-    </div>
-        
-<?php endforeach ?>
-<?php endif ?>
-        
-  
-        
-<div id="cart-total">       
-<div class="totalen">
-    <?php echo 'Summa varor';?>
-</div>
+                <!-- Produktbild -->
+                <div class="item-img">
+                    <img id="cart_img" src="<?php echo $cartItemData['img_url'];?>">
+                </div>
                 
-<div class="cart-total">
-    <?php echo $cart['total'].' KR';?>
-    <br>
-    <button class="update-cart-btn" type="submit">
-                            Uppdatera varukorgen</button> 
-</div>
-</div>                  
-                     
-</div> 
-            
-</form>
-    
-     <div class="btn-box">
+                <!-- Produkttitel -->   
+                <div class="item-title">
+                    <span class="desc"><?php echo $cartItemData['title'];?></span>
+                </div>
+                
+                <!-- Antal valda  produkter -->     
+                <div class="item-qty">
+                    <input type="number" name="cartitems[<?php echo $cartItemPid;?>]" value="<?php echo $cartItemData['qty'];?>">
+                </div>
+                
+                <!-- Total summa för en vara -->
+                <div class="item-price">
+                    <?php echo $cartItemData['price'].' SEK';?>
+                </div>
+                        
+                <!-- Total summa för en vara x antal-->    
+                <div class="cart-sum">
+                    <?php echo $cartItemData['sum'].' SEK';?>
+
+                <!-- Remove cart item -->
+                <a href="?action=removecartitem&pid=<?php echo $cartItemPid;?>">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                </a>
+            </div> <!-- /content -->
+
+    </div>  
+        
+            <?php endforeach ?>
+            <?php endif ?>
+          
+        <div id="cart-total">       
+            <div class="total">
+                <?php echo 'Ordersumma';?>
+            </div>
+                        
+            <div class="cart-total">
+                <?php echo $cart['total'].' SEK';?>
+                <br>
+                <button class="update-cart-btn" type="submit"> Uppdatera varukorgen </button> 
+            </div>
+        </div>                                
+        </div>       
+    </form>
+    </div>    
+
+    <!-- Aside bredvid varukorg -->
+    <div class="aside"> Har du frågor om din order?
+        <ul>
+            <li><i class="fa fa-phone" aria-hidden="true"></i> <span id="hej"> 020 - 53 53 52 </span></li>
+        </ul>
+    </div>
+    <!-- Checkout knapp -->
+    <div class="btn-box">
         <form method="post" action="?action=checkout" name="form-checkout-btn">
             <button type="submit" id="checkoutknapp" name="place-order" 
-            value="<?php $cartItemData;?>">Lägg order</button>
+            value="<?php $cartItemData;?>"> Fortsätt till betalning </button>
          </form>
     </div>
- </div>    
-<?php 
-require_once('templates/footer.php');
-?>
+
+</div> <!-- The wrapper -->
