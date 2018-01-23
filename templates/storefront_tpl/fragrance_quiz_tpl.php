aefa29b3481107b70dc077b6758d16d8c0433946
katerinanikou <katerinanikou1@gmail.com>
2018-01-23 17:58:47 +0100

    Första utkast quiz m. bilder

--- templates/storefront_tpl/fragrance_quiz_tpl.php
+++ templates/storefront_tpl/fragrance_quiz_tpl.php
@@ -2,4 +2,26 @@
 require_once('templates/storefront_tpl/header.php');
 ?>
 
-Här kommer quizzet vara
+<!-- QUIZ "Vilken doft är du" -->
+<link type="text/css" rel="stylesheet" href="css/storefront/fragrance_quiz.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+
+
+
+<div class="main_wrapper">
+    <!--Canvas -->
+    <div class="white-canvas">
+        <div class="thequiz">
+            <h2> Så berätta för oss, när vill du använda doften? </h2>
+            <div class="container">
+                  <div><img src="img/quiz/daytime.jpg"><p class="one"> Dagtid </p></div>
+                  <div><img src="img/quiz/nighttime.jpg"><p class="two"> Kvällstid </p></div>
+                  <div><img src="img/quiz/versatile.jpg"><p class="three"> Både och </p></div> 
+            </div>
+                     
+                     
+                    
+        </div>
+    </div>
+</div>
