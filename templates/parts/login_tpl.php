2bd1ab8bd424632c23abc39c5cea4d073a1309b9
katerinanikou <katerinanikou1@gmail.com>
2018-01-25 22:58:00 +0100

    Login klar css

--- templates/parts/login_tpl.php
+++ templates/parts/login_tpl.php
@@ -6,39 +6,43 @@ include('templates/storefront_tpl/header.php');
 
 
 <div class="main_wrapper">
-    <div class="login">
-        
+    
+    <div class="login"> 
+
         <!-- Inputfält logga in -->
         <h2 class="login_title">
             HEJ, <?php echo $pagecontent->h2 ?>!
         </h2>
-        <form action="" method="post" autocomplete="off" name="login">
-            <input class="login" type="text" name="email" placeholder="E-postadress">
-            <br>
-            <input class="login" id="passinput" type="password" name="input_password" placeholder="Lösenord">
-            <br>
+            <form action="" method="post" autocomplete="off" name="login">
+                <input class="login" type="text" name="email" placeholder="E-postadress">
+                <br>
+                <input class="login" id="passinput" type="password" name="input_password" placeholder="Lösenord">
+                <br>
 
-            <!-- FORM TOKEN-->
-            <?php echo getTokenField(); ?>
-
-            <!-- LOGGA IN-KNAPP-->
-            <button type="submit" id="login">LOGGA IN</button>
-        </form>
-    </div>
-
-    <div class="create_account">
-        <!--INPUTFÄLT LOGGA IN-->
-        <img class="create_canvas" src="img/inspo/tulipan_800x800.jpg">
-
-        <div class="on_top">
-            <h2 class="create_title"> Bli medlem </h2>
-            <form class="create_account" action="?action=create_user" method="post" autocomplete="off">
-
+                <!-- FORM TOKEN-->
+                <?php echo getTokenField(); ?>
 
                 <!-- LOGGA IN-KNAPP-->
-                <button type="submit" id="send" name="submit_button">SKAPA KONTO</button>
+                <button type="submit" id="login">LOGGA IN</button>
+            </form>
+
+    <div class="on_top">
+                <hr>
+
+        <h2 class="create_title"> Inte medlem?</h2>
+            <form class="create_account" action="?action=create_user" method="post" autocomplete="off">
+
+                <!-- Logga-in knapp -->
+                <button type="submit" id="send" name="submit_button">ANSÖK HÄR</button>
                 <br>
             </form>
         </div>
+    </div>
+
+    <div class="create_account">
+        <!-- Stor bild till höger -->
+        <img class="create_canvas" src="img/inspo/tulipan_800x800.jpg">
+
+       
     </div>
 </div>
