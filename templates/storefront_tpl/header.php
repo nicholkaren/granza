c8d2e067c5a9db196695ceb539b8baeeb174ff4f
katerinanikou <katerinanikou1@gmail.com>
2018-02-03 15:03:38 +0100

    sticky meny
    Bootstrap meny
    funkar helt ok i mobil just nu
    Vi avvaktar med logotyp

--- templates/storefront_tpl/header.php
+++ templates/storefront_tpl/header.php
@@ -5,21 +5,46 @@
 <link rel="shortcut icon" href="img/logo/granza.png"/>
 <link type="text/css" rel="stylesheet" href="css/storefront/header.css">
 <link type="text/css" rel="stylesheet" href="css/storefront/footer.css">
+
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800|Work+Sans:300,400" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-<meta name="viewport" content="width=device-width, initial-scale=1">  
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
 
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <ul class="nav navbar-nav">
+      <li><a href="?action=default">Hem</a></li>
+      <li><a href="?action=category&id=1">Damdofter</a></li>
+      <li><a href="?action=category&id=2">Herrdofter</a></li>
+    </ul>
+
+    <ul class="nav navbar-nav navbar-right">
+              <li><a href="?action=about-us">Om Granza</a></li>
+
+      <li><a href="?action=login"><span class="glyphicon glyphicon-user"></span></a></li>
+      <li><a href="?action=addtocart"><span id="shop_bag"> <i class="fa fa-shopping-bag" aria-hidden="true"></i> 
+
+            <!-- Skriver ut antalet produkter i varukorgen -->
+            <?php if(isset($_SESSION['cart-total']) && $_SESSION['cart-total'] > 0){
+                printf ('<div id="qty-cart"><span id="qty">%s</span></div>', $_SESSION['cart-total']);
+            }
+            ?>
+            </span></a></li>
+    </ul>
+</nav>
 
 <!--Header -->
-<header>
 
-    <!--Meny som fälls ut -->
+
+
+
+    <!--Meny som fälls ut 
       <input type="checkbox" id="nav-trigger" class="nav-trigger">
         <label for="nav-trigger" class="toggle">☰</label>
         <nav class="menu">
@@ -31,23 +56,23 @@
                 <li><a href="?action=about-us">Om oss</a></li>
                 <li><a href="?action=default">Hem</a></li>
             </ul>
-        </nav>
+        </nav>-->
 
-    <!-- Logotyp -->
+    <!-- Logotyp 
     <div class="div_logo">
         <a href="?action=default">
         <img src="img/logo/granza.png" alt="logotype granza" id="id_logo"></a>
-    </div>
+    </div>-->
     
-    <!-- Ikoner till Logga in som admin/member och shoppingbag-->
+    <!-- Ikoner till Logga in som admin/member och shoppingbag
     <div class="menu_icons">
-        <!-- Login icon -->
+        Login icon
         
         <a href="?action=login"><i class="fa fa-user" aria-hidden="true"></i></a>
-        <!-- Shopping bad icon-->
+        Shopping bad icon
         <a href="?action=addtocart">
             <span id="shop_bag">
-                <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                <i class="fa fa-shopping-bag" aria-hidden="true"></i>-->
                 
             <!-- Skriver ut antalet produkter i varukorgen -->
             <?php if(isset($_SESSION['cart-total']) && $_SESSION['cart-total'] > 0){
@@ -56,4 +81,3 @@
                 ?>
             </span></a>
     </div> 
-</header>
