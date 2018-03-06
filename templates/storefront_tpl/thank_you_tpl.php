dafbdf355cf2c70733d7135e2254c46170173135
katerinanikou <katerina.nikou@sunnyatsea.se>
2018-03-06 17:25:52 +0100

    Lite småpill på thankyou

--- templates/storefront_tpl/thank_you_tpl.php
+++ templates/storefront_tpl/thank_you_tpl.php
@@ -5,14 +5,14 @@
 
 	<img class="background_flowers" src="img/products/big/thankyou.jpg">  
 	<div class="thank_you_wrapper">
-		<h1 id="thanks-h1"> Grazie! Tack för ditt köp!</h1>
+		<h1 id="thanks-h1"> Grazie! Tack för att du handlar hos oss på Granza.</h1>
 		<p id="thanks-p"> 
-			Vi skickar en orderbekräftelse till dig via e-post inom kort. <br>
-			Har du några frågor så får du gärna höra av dig till oss. <br><br>
-        	<i class="fa fa-phone" aria-hidden="true"></i> <span id="order"> 020 - 53 53 52 </span></li>
+			Vi skickar en orderbekräftelse till dig via mejl inom kort, där finner du mer information om din order.<br> Vi hoppas att du blir nöjd med din beställning.
+			Har du några frågor så är du varmt välkommen att höra av dig till oss.<br><br>
+        	<i class="fa fa-phone" aria-hidden="true"></i><a href="tel:020535352"> <span id="order">020 - 53 53 52 </span></a></li>
 		</p>
 	</div>
 <?php 
-    
+
     require_once('templates/storefront_tpl/footer.php');
 ?>
